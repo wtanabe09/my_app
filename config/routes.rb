5bc6be97a2bb7bf03cadd8191564d30efb5eb115
watanabe <w.shoya.0910@gmail.com>
2021-03-11 12:38:15 +0900

    finish create function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root 'homes#top'
   resources :posts
+  resources :users, only: [:show, :edit, :update]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_070951) do
+ActiveRecord::Schema.define(version: 2021_03_11_032140) do
 
   create_table "posts", force: :cascade do |t|
     t.text "title"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_03_03_070951) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
